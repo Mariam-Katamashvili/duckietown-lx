577820d192357e402f07392e82c89c88384d6a20
Mariam Katamashvili <katamashvili.mariami@gmail.com>
2025-04-03 12:54:42 +0400

    finished exercise

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -3,20 +3,27 @@ from typing import Tuple
 import numpy as np
 
 
-def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
-    # TODO: write your function instead of this one
-    res = np.zeros(shape=shape, dtype="float32")
-    # these are random values
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
-    # ---
-    return res
+import math
 
+import numpy as np
+from typing import Tuple
+
+def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
+    h, w = shape
+    mid = w / 2.0
+
+    rows = np.arange(h).reshape(h, 1)   
+    cols = np.arange(w).reshape(1, w)    
+
+    vertical_weight = rows / float(h)
+    
+    directional = (cols - mid) / mid
+
+    sigma = w * 0.15  
+    center_mask = np.exp(-((cols - mid) ** 2) / (2 * sigma ** 2))
+    
+    left_matrix = vertical_weight * directional * center_mask
+    return left_matrix.astype(np.float32)
 
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
-    # TODO: write your function instead of this one
-    res = np.zeros(shape=shape, dtype="float32")
-    # these are random values
-    res[100:150, 100:300] = -1
-    # ---
-    return res
+    return np.fliplr(get_motor_left_matrix(shape)).astype(np.float32)
